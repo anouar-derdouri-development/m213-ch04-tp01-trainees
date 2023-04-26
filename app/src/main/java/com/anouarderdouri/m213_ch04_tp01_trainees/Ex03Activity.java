ee011f5035668806173e2b8a0905ee75e600fcc0
Anouar DERDOURI <anouar.derdouri.development@gmail.com>
2023-04-26 16:26:21 +0000

    Ex03: Done

---
+++ app/src/main/java/com/anouarderdouri/m213_ch04_tp01_trainees/Ex03Activity.java
@@ -0,0 +1,14 @@
+package com.anouarderdouri.m213_ch04_tp01_trainees;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ex03Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ex03);
+    }
+}
