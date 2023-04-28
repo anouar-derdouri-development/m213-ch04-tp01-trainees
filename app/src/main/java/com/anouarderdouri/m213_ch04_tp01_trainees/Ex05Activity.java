5c46d8d4d831ecf761488e3c4d7aed18b33cd6f3
Anouar DERDOURI <anouar.derdouri.development@gmail.com>
2023-04-28 17:24:26 +0000

    Ex05: Done

--- app/src/main/java/com/anouarderdouri/m213_ch04_tp01_trainees/Ex05Activity.java
+++ app/src/main/java/com/anouarderdouri/m213_ch04_tp01_trainees/Ex05Activity.java
@@ -3,15 +3,12 @@ package com.anouarderdouri.m213_ch04_tp01_trainees;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.LinearLayout;
 
-public class Ex04Activity extends AppCompatActivity {
+public class Ex05Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ex04);
-
-
+        setContentView(R.layout.activity_ex05);
     }
 }
